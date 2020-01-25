5689621a9909a5fe817d32f39736ead7d97b65da
John Moody <john@mentalvelocity.com>
2020-01-25 08:10:39 -0800

    Added readme

---
+++ README.mk
@@ -0,0 +1,13 @@
+# Vision2020
+## Testing for FRC 2020 season
+
+## Useful websites
+- [Chessboard calibration technique](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html)
+- [OpenCV threading technique](https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/)
+
+## Current plan
+- Determine POV of camera
+- Get video streaming
+- Set up app structure
+- Calculate target coordinates
+- Stream target coordinates via UDP
